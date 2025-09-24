34253033bd18d4be800273ef9dd256a57163958e
Florian Bardin <fbardin51@gmail.com>
2025-09-24 23:54:29 +0200

    Disposition et styles par défaut

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="flex flex-col p-3 gap-3">
+      <div className="flex flex-row justify-between items-center">
+        <h3 className="font-semibold">Task Flow</h3>
+        <nav>
+          <select name="kanban" id="kanban">
+            <option value="school">School</option>
+            <option value="perso">Perso</option>
+          </select>
+        </nav>
+      </div>
+      <div className="bg-white p-6 rounded-2xl border border-gray-200">
+        <h1 className="font-medium">Project UI/UX</h1>
+      </div>
+    </main>
+  );
 };
 
 export default App;
