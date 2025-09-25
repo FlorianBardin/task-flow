a213a5946d4459d78a9954172085688e8e2612a0
Florian Bardin <fbardin51@gmail.com>
2025-09-25 19:36:58 +0200

    Typage des données du Kanban

---
+++ types/index.d.ts
@@ -0,0 +1,23 @@
+export interface Kanban {
+  id: number;
+  name: string;
+  description?: string;
+  columns: Column[];
+  createdAt?: Date;
+}
+
+export interface Column {
+  id: string;
+  name: "todo" | "doing" | "toreview" | "done";
+  title: string;
+  tasks: Task[];
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  priority?: "low" | "medium" | "high";
+  assignee?: string;
+  dueDate?: Date;
+}
