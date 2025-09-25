ee6e9cd885bd6fbfa0785e2694971e06be2f3f53
Florian Bardin <fbardin51@gmail.com>
2025-09-25 22:45:59 +0200

    Améliration du composant de colonne

--- src/components/KanbanColumn.tsx
+++ src/components/KanbanColumn.tsx
@@ -1,9 +1,20 @@
-type Props = {
-  children?: React.ReactNode;
-};
+import type { Column } from "../../types/index";
+import KanbanTask from "./KanbanTask";
 
-const KanbanColumn = ({ children }: Props) => {
-  return <div>{children}</div>;
+const KanbanColumn = (column: Column) => {
+  return (
+    <div className="bg-gray-100 p-4 rounded-xl">
+      <div className="flex justify-between">
+        <h4 className="font-medium">{column.title}</h4>
+        <p>+</p>
+      </div>
+      <div className="flex flex-col gap-3">
+        {column.tasks.map((task) => {
+          return <KanbanTask {...task} key={task.id} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default KanbanColumn;
