33c82c7f24ddb8b2f4c95043c281099c18cbe883
Florian Bardin <fbardin51@gmail.com>
2025-09-26 23:54:35 +0200

    Ajout des boutons Edit et Add

--- src/components/KanbanColumn.tsx
+++ src/components/KanbanColumn.tsx
@@ -1,12 +1,22 @@
 import type { Column } from "../../types/index";
 import KanbanTask from "./KanbanTask";
+import add from "../assets/add.svg";
+import modify from "../assets/modify.svg";
 
 const KanbanColumn = (column: Column) => {
   return (
-    <div className="bg-gray-100 p-4 rounded-xl">
-      <div className="flex justify-between">
-        <h4 className="font-medium">{column.title}</h4>
-        <p>+</p>
+    <div className="flex flex-col bg-gray-100 p-4 rounded-xl gap-2">
+      <div className="flex p-3 items-center justify-between">
+        <div className="flex items-center gap-2">
+          <h4 className="font-medium">{column.title}</h4>
+          <div className="flex justify-center items-center bg-gray-200 text-gray-400 text-sm h-5.5 w-5.5 px-2 rounded-sm">
+            {column.tasks.length}
+          </div>
+        </div>
+        <div className="flex flex-row-reverse h-4 w-4 gap-4">
+          <img src={add} alt="Add button" />
+          <img src={modify} alt="Modify button" />
+        </div>
       </div>
       <div className="flex flex-col gap-3">
         {column.tasks.map((task) => {
