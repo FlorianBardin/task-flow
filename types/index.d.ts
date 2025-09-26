3e4861c774d23a93f6f8b8c6c6c275f83d1c229a
Florian Bardin <fbardin51@gmail.com>
2025-09-26 23:53:53 +0200

    Fix des types de Task

--- types/index.d.ts
+++ types/index.d.ts
@@ -17,7 +17,7 @@ export interface Task {
   id: number;
   title: string;
   description?: string;
-  priority?: "low" | "medium" | "high";
-  assignee?: string[];
+  priority: "low" | "medium" | "high";
+  assignee: string[];
   dueDate?: Date;
 }
