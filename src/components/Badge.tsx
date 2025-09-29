ae620f22ae1f6e89ab3d09b92973d592c0ac8521
Florian Bardin <fbardin51@gmail.com>
2025-09-29 11:31:46 +0200

    Màj visuel des badges de priorité

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,13 +1,13 @@
 type BadgeType = {
   children: React.ReactNode;
-  color: "low" | "medium" | "high" | "assign";
+  color: "Low" | "Medium" | "High" | "assign";
 };
 
 const Badge = ({ children, color }: BadgeType) => {
   const colors = {
-    high: "text-red-700 bg-red-300",
-    medium: "text-yellow-700 bg-yellow-300",
-    low: "text-green-700 bg-green-300",
+    High: "text-red-700 bg-red-300",
+    Medium: "text-yellow-700 bg-yellow-300",
+    Low: "text-green-700 bg-green-300",
     assign: "text-blue-600 bg-blue-200",
   };
 
