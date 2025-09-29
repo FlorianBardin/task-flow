04abb641908dce903382a6fb9c7784657c750bca
Florian Bardin <fbardin51@gmail.com>
2025-09-29 11:32:11 +0200

    Ajout de clé pour chaque assigné

--- src/components/KanbanTask.tsx
+++ src/components/KanbanTask.tsx
@@ -12,8 +12,12 @@ const KanbanTask = (task: Task) => {
       <p className="text-gray-600">{task.description}</p>
       <div className="flex gap-2 items-center mt-1.5 flex-wrap">
         <img className="h-4" src={assigned} alt="" />
-        {task.assignee?.map((name) => {
-          return <Badge color="assign">{name}</Badge>;
+        {task.assignee.map((assignee) => {
+          return (
+            <Badge color="assign" key={assignee.id}>
+              {assignee.name}
+            </Badge>
+          );
         })}
       </div>
       <p className="text-gray-400 mt-5">{task.dueDate?.toDateString()}</p>
