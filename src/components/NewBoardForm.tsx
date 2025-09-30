6e9219040017bfa603e23591e171e0f08d8c616f
Florian Bardin <fbardin51@gmail.com>
2025-09-30 22:56:58 +0200

    Création d'un formulaire de création de tableau

---
+++ src/components/NewBoardForm.tsx
@@ -0,0 +1,55 @@
+import Button from "./Button";
+import type { Kanban } from "../../types";
+import { emptyBoard } from "../../data/emptyBoard";
+
+type NewBoardForm = {
+  setIsOpen: (isOpen: boolean) => unknown;
+  kanbanStorage: Kanban[] | undefined;
+  setKanbanStorage: (kanbans: Kanban[]) => unknown;
+};
+
+const NewBoardForm = ({
+  setIsOpen,
+  kanbanStorage,
+  setKanbanStorage,
+}: NewBoardForm) => {
+  const insertNewBoard = (formData: FormData) => {
+    const name: string = String(formData.get("name"));
+    const description: string = String(formData.get("description"));
+    const newBoard = emptyBoard;
+    newBoard.id = kanbanStorage!.length + 1;
+    newBoard.name = name;
+    newBoard.description = description;
+    newBoard.createdAt = new Date();
+    setKanbanStorage([...kanbanStorage!, newBoard]);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center">
+      <div className="w-[90%] max-w-[700px] flex flex-col gap-4 bg-white p-8 rounded-2xl border border-gray-200 drop-shadow-xl">
+        <h2 className="font-medium">New board</h2>
+        <form action={insertNewBoard} className="flex flex-col gap-4">
+          <label className="flex flex-col gap-2">
+            Name your board
+            <input className="input" name="name" type="text" />
+          </label>
+          <label className="flex flex-col gap-2">
+            Describe it
+            <input className="input" name="description" type="text" />
+          </label>
+          <div className="flex justify-between">
+            <Button variant="destructive" onClick={() => setIsOpen(false)}>
+              Annuler
+            </Button>
+            <Button type="submit" variant="primary">
+              Valider
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default NewBoardForm;
