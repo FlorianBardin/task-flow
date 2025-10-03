7e335f55eddc90333d282b00f68b550d461bd1c8
Florian Bardin <fbardin51@gmail.com>
2025-10-03 22:18:22 +0200

    Implémentation du context dans l'app

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Button from "./components/Button";
 import NewBoardForm from "./components/NewBoardForm";
 import { useLocalStorage } from "react-use";
 import NewTaskForm from "./components/NewTaskForm";
+import { KanbanContext } from "./contexts/KanbanContext";
 
 const App = () => {
   const [kanbanStorage, setKanbanStorage] = useLocalStorage(
@@ -15,67 +16,76 @@ const App = () => {
 
   return (
     <main className="flex flex-col p-4 gap-4 xl:min-h-[100vh]">
-      <button
-        className="cursor-pointer"
-        onClick={() => {
-          localStorage.clear();
+      <KanbanContext.Provider
+        value={{
+          activeKanban: activeKanban,
+          setActiveKanban: setActiveKanban,
+          kanbanStorage: kanbanStorage,
+          setKanbanStorage: setKanbanStorage,
         }}
       >
-        Clear local storage
-      </button>
-      <div className="flex justify-between items-start sm:items-center px-4 gap-3">
-        <img className="w-9.5" src="task-flow-logo.svg" alt="" />
-        <div className="flex flex-col sm:flex-row-reverse items-end gap-2">
-          <NewBoardForm
-            kanbanStorage={kanbanStorage}
-            setKanbanStorage={setKanbanStorage}
-          />
-          <nav>
-            <select
-              name="kanban"
-              id="kanban"
-              className="text-gray-700 inset-ring inset-ring-gray-300 p-2 px-4 h-10 w-fit rounded-lg cursor-pointer"
-              onChange={(e) =>
-                setActiveKanban(
-                  kanbanStorage!.find(
-                    (kanban) => kanban.id === Number(e.target.value)
-                  )!
-                )
-              }
-            >
-              {kanbanStorage!.map((kanban) => {
-                return (
-                  <option value={kanban.id} key={kanban.id}>
-                    {kanban.name}
-                  </option>
-                );
-              })}
-            </select>
-          </nav>
+        <button
+          className="cursor-pointer"
+          onClick={() => {
+            localStorage.clear();
+          }}
+        >
+          Clear local storage
+        </button>
+        <div className="flex justify-between items-start sm:items-center px-4 gap-3">
+          <img className="w-9.5" src="task-flow-logo.svg" alt="" />
+          <div className="flex flex-col sm:flex-row-reverse items-end gap-2">
+            <NewBoardForm
+              kanbanStorage={kanbanStorage}
+              setKanbanStorage={setKanbanStorage}
+            />
+            <nav>
+              <select
+                name="kanban"
+                id="kanban"
+                className="text-gray-700 inset-ring inset-ring-gray-300 p-2 px-4 h-10 w-fit rounded-lg cursor-pointer"
+                onChange={(e) =>
+                  setActiveKanban(
+                    kanbanStorage!.find(
+                      (kanban) => kanban.id === Number(e.target.value)
+                    )!
+                  )
+                }
+              >
+                {kanbanStorage!.map((kanban) => {
+                  return (
+                    <option value={kanban.id} key={kanban.id}>
+                      {kanban.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </nav>
+          </div>
         </div>
-      </div>
 
-      <div className="flex flex-col gap-6 bg-white p-8 rounded-2xl border border-gray-200 flex-1">
-        <div className="flex flex-col gap-2">
-          <h1 className="font-medium">{activeKanban.name}</h1>
-          <p className="text-gray-600">{activeKanban.description}</p>
+        <div className="flex flex-col gap-6 bg-white p-8 rounded-2xl border border-gray-200 flex-1">
+          <div className="flex flex-col gap-2">
+            <h1 className="font-medium">{activeKanban.name}</h1>
+            <p className="text-gray-600">{activeKanban.description}</p>
+          </div>
+          <div className="flex gap-2">
+            <NewTaskForm
+              kanbanStorage={kanbanStorage}
+              setKanbanStorage={setKanbanStorage}
+              activeKanban={activeKanban}
+              setActiveKanban={setActiveKanban}
+            />
+            <Button variant="secondary">Change WIP limit</Button>
+          </div>
+          <div className="flex flex-col gap-4 xl:flex-row">
+            {activeKanban.columns.map((column) => {
+              return <KanbanColumn column={column} key={column.id} />;
+            })}
+          </div>
+          <Button variant="destructive">Delete kanban</Button>
         </div>
-        <div className="flex gap-2">
-          <NewTaskForm
-            kanbanStorage={kanbanStorage}
-            setKanbanStorage={setKanbanStorage}
-            activeKanban={activeKanban}
-            setActiveKanban={setActiveKanban}
-          />
-          <Button variant="secondary">Change WIP limit</Button>
-        </div>
-        <div className="flex flex-col gap-4 xl:flex-row">
-          {activeKanban.columns.map((column) => {
-            return <KanbanColumn column={column} key={column.id} />;
-          })}
-        </div>
-        <Button variant="destructive">Delete kanban</Button>
-      </div>
+      </KanbanContext.Provider>
     </main>
   );
 };
